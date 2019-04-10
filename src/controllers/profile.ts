866541aae13964ac5d99b65b131c1730046e8c5d
Spinelli Danilo <danilo.spinelli@agid.gov.it>
2019-04-10 14:52:22 +0200

    profile

---
+++ src/controllers/profile.ts
@@ -0,0 +1,36 @@
+/**
+ * This controller handles reading the user profile from the
+ * app by forwarding the call to the API system.
+ */
+
+import * as express from "express";
+import { isLeft } from "fp-ts/lib/Either";
+import {
+  IResponseErrorInternal,
+  IResponseSuccessJson,
+  ResponseErrorInternal,
+  ResponseSuccessJson
+} from "italia-ts-commons/lib/responses";
+
+import { extractUserFromRequest, User } from "../types/user";
+
+export default class ProfileController {
+  /**
+   * Returns the profile for the user identified by the provided fiscal
+   * code.
+   */
+  public async getProfile(
+    req: express.Request
+  ): Promise<IResponseSuccessJson<User> | IResponseErrorInternal> {
+    const errorOrUser = extractUserFromRequest(req);
+
+    if (isLeft(errorOrUser)) {
+      // Unable to extract the user from the request.
+      const error = errorOrUser.value;
+      return ResponseErrorInternal(error.message);
+    }
+
+    const profile = errorOrUser.value;
+    return ResponseSuccessJson(profile);
+  }
+}
