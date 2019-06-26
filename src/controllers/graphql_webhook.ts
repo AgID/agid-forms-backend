c643d6832b2ca91dabc6f3ea71376bca716dbde8
Spinelli Danilo <danilo.spinelli@agid.gov.it>
2019-06-26 15:00:22 +0200

    webhook payload

--- src/controllers/graphql_webhook.ts
+++ src/controllers/graphql_webhook.ts
@@ -15,11 +15,54 @@ import {
   wrapRequestHandler
 } from "italia-ts-commons/lib/request_middleware";
 
+import { NonNegativeInteger } from "italia-ts-commons/lib/numbers";
+import { NonEmptyString } from "italia-ts-commons/lib/strings";
+import { UUIDString } from "../generated/api/UUIDString";
 import { RequiredHeaderValueMiddleware } from "../middlewares/required_header_value";
 
+import { UTCISODateFromString } from "italia-ts-commons/lib/dates";
+import { DecodeBodyMiddleware } from "../middlewares/decode_body";
+
+const NodeT = t.interface({
+  content: t.object,
+  created_at: UTCISODateFromString,
+  id: UUIDString,
+  language: NonEmptyString,
+  status: NonEmptyString,
+  title: NonEmptyString,
+  type: NonEmptyString,
+  updated_at: UTCISODateFromString,
+  user_id: UUIDString,
+  version: NonNegativeInteger
+});
+
 // tslint:disable-next-line: no-commented-code
-// const WebhookPayload = t.interface({});
-// type WebhookPayload = t.TypeOf<typeof WebhookPayload>;
+const WebhookPayload = t.interface({
+  event: t.interface({
+    created_at: UTCISODateFromString,
+    data: t.partial({
+      new: NodeT,
+      old: NodeT
+    }),
+    delivery_info: t.interface({
+      current_retry: NonNegativeInteger,
+      max_retries: NonNegativeInteger
+    }),
+    id: UUIDString,
+    op: t.union([t.literal("UPDATE"), t.literal("INSERT")]),
+    session_variables: t.partial({
+      "x-hasura-role": NonEmptyString
+    }),
+    table: t.interface({
+      name: NonEmptyString,
+      schema: NonEmptyString
+    }),
+    trigger: t.interface({
+      name: NonEmptyString
+    })
+  })
+});
+type WebhookPayload = t.TypeOf<typeof WebhookPayload>;
 
 const WebhookResponse = t.interface({
   message: t.string
@@ -52,7 +95,8 @@ export function GraphqlWebhook(
 ): express.RequestHandler {
   const handler = GraphqlWebhookHandler(receivedWebhookToken);
   const withrequestMiddlewares = withRequestMiddlewares(
-    RequiredHeaderValueMiddleware("x-webhook-token")
+    RequiredHeaderValueMiddleware("x-webhook-token"),
+    DecodeBodyMiddleware(WebhookPayload)
   );
   return wrapRequestHandler(withrequestMiddlewares(handler));
 }
