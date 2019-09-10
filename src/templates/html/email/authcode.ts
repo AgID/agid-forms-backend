83fdbbe337539c5912667078d0ed38a1885883c6
gunzip <danilo.spinelli@agid.gov.it>
2019-09-10 15:31:38 +0200

    #168363168 email ipa code

--- src/templates/html/email/authcode.ts
+++ src/templates/html/email/authcode.ts
@@ -1,19 +1,27 @@
 import { format } from "date-fns";
 
-export const emailAuthCode = (secretCode: string, ipaName: string) => ({
+export const emailAuthCode = (
+  secretCode: string,
+  ipaName: string,
+  ipaCode: string
+) => ({
   html: `
-    <p>Ciao,<br />
-    il ${format(new Date(), "DD/MM/YYYY")} alle ore ${format(
+  <p>All’attenzione dell’RTD dell’Amministrazione '${ipaName}',<br />
+  in data ${format(new Date(), "DD/MM/YYYY")} alle ore ${format(
     new Date(),
     "HH:mm"
-  )} è stata effettuata una richiesta di accesso all'<b>applicazione
-    per la dichiarazione di accessibilità dei siti web e delle applicazioni mobili</b>
-    relativa all'amministrazione '${ipaName}' per la quale il presente 
-    indirizzo e-mail è indicato come referente RTD.</p>
-      <br />
-    <p>Il codice di accesso all'applicazione é il seguente:</p>
-      <br />
-    <h2>${secretCode}</h2>
+  )} è stata effettuata una richiesta di accesso al servizio on line di AGID
+  per la compilazione della dichiarazione di accessibilità di tutti siti web
+  e delle applicazioni mobili della vostra amministrazione.
+  </p>
+  <p>
+  Per accedere al servizio è necessario collegarsi alla seguente pagina 
+  [https://form.agid.gov.it](https://form.agid.gov.it/?ipa=${ipaCode})
+  inserendo la chiave di accesso:
+  </p>
+  <h2>${secretCode}</h2>
+  <p>Distinti saluti,</p>
+  <p>AGID</p>
   `,
   title: `Dichiarazione di accessibilità - codice d'accesso`
 });
