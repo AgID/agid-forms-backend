88a3ecb9637d85874944dc8c58725bc7e7ebfb05
gunzip <danilo.spinelli@agid.gov.it>
2019-11-05 15:20:17 +0100

    upload-server

--- src/uploads/upload-server.ts
+++ src/uploads/upload-server.ts
@@ -7,9 +7,9 @@ import {
   MINIO_ACCESS_KEY,
   MINIO_DEFAULT_BUCKETS,
   MINIO_DEFAULT_REGION,
-  MINIO_ENDPOINT,
-  MINIO_PORT,
   MINIO_SECRET_KEY,
+  MINIO_SERVER_HOST,
+  MINIO_SERVER_PORT_NUMBER,
   UPLOAD_SERVER_PORT
 } from "../config";
 import { log } from "../utils/logger";
@@ -25,7 +25,8 @@ export interface IFileType {
 }
 
 // TODO: real database
-const dbGlobal: Record<string, IFileType> = {};
+// tslint:disable-next-line: readonly-array
+const dbGlobal: IFileType[] = [];
 
 /**
  * Stores a GraphQL file upload.
@@ -34,52 +35,62 @@ const getStoreUpload = (minioClient: Minio.Client) => async (
   upload: FileUpload,
   bucketName?: string
 ): Promise<IFileType> => {
-  const { createReadStream, filename, mimetype } = upload;
+  try {
+    const { createReadStream, filename, mimetype } = upload;
 
-  // TODO: create file node in hasura and get back
-  // node.id (upsert status=draft)
-  const id = ulid();
+    // TODO: create file node in hasura and get back
+    // node.id (upsert status=draft) and node.user.id
+    const id = ulid();
 
-  const computedBucketName = bucketName || MINIO_DEFAULT_BUCKETS.split(",")[0];
+    // TODO: bucketName must start with the current node.user.id
 
-  // create bucket if not exists
-  const bucketExists = await minioClient.bucketExists(computedBucketName);
-  if (!bucketExists) {
-    log.info("bucket '%s' does not exists, try to create it", bucketName);
-    await minioClient.makeBucket(computedBucketName, MINIO_DEFAULT_REGION);
+    const computedBucketName =
+      bucketName || MINIO_DEFAULT_BUCKETS.split(",")[0];
+    log.info("getStoreUpload: handling '%s' %s", computedBucketName, filename);
+
+    // create bucket if not exists
+    const bucketExists = await minioClient.bucketExists(computedBucketName);
+    if (!bucketExists) {
+      log.info("bucket '%s' does not exists, try to create it", bucketName);
+      await minioClient.makeBucket(computedBucketName, MINIO_DEFAULT_REGION);
+    } else {
+      log.info("bucket '%s' exists", bucketName);
+    }
+
+    const metaData = {
+      filename,
+      id,
+      mimetype
+    };
+
+    // store the file in minio
+    await minioClient.putObject(
+      computedBucketName,
+      filename,
+      createReadStream(),
+      undefined, // size
+      metaData
+    );
+
+    const file = { id, filename, mimetype };
+
+    // TODO: update file metadata in hasura (upsert status=published)
+    dbGlobal.push(file);
+
+    return file;
+  } catch (e) {
+    log.info("storeUpload error: %s", JSON.stringify(e));
+    throw e;
   }
-
-  const metaData = {
-    filename,
-    id,
-    mimetype
-  };
-
-  // store the file in minio
-  await minioClient.putObject(
-    computedBucketName,
-    filename,
-    createReadStream(),
-    undefined, // size
-    metaData
-  );
-
-  const file = { id, filename, mimetype };
-
-  // TODO: update file metadata in hasura (upsert status=published)
-  // tslint:disable-next-line: no-object-mutation
-  dbGlobal[id] = file;
-
-  return file;
 };
 
 export type StoreUploadT = ReturnType<typeof getStoreUpload>;
 
 const typeDefs = gql`
   type File {
+    id: String!
     filename: String!
     mimetype: String!
-    encoding: String!
   }
 
   type Query {
@@ -92,7 +103,8 @@ const typeDefs = gql`
 `;
 
 interface IGraphqlUploadContext {
-  db: Record<string, IFileType>;
+  // tslint:disable-next-line: readonly-array
+  db: IFileType[];
   storeUpload: StoreUploadT;
 }
 
@@ -102,11 +114,13 @@ const resolvers = {
       // tslint:disable-next-line: no-any
       _: any,
       { file, bucketName }: { file: Promise<FileUpload>; bucketName: string },
-      { db, storeUpload }: IGraphqlUploadContext
-    ) => {
+      { storeUpload }: IGraphqlUploadContext
+    ): Promise<IFileType> => {
       const fileObj = await file;
+      log.info("singleUpload: bucket=%s file=%s", bucketName, file);
       const meta = await storeUpload(fileObj, bucketName);
-      log.info("source (%s) args=(%s)", meta, db);
+      log.info("singleUpload: meta=%s", JSON.stringify(meta));
+      return meta;
     }
   },
   Query: {
@@ -116,9 +130,10 @@ const resolvers = {
       // tslint:disable-next-line: no-any
       args: any,
       { db }: IGraphqlUploadContext
-    ) => {
-      // TODO: get metadata from minio (or DB) for each file
-      log.info("source (%s) args=(%s)", args, db);
+    ): // tslint:disable-next-line: readonly-array
+    Promise<IFileType[]> => {
+      // TODO: get metadata from hasura
+      log.info("uploads: source (%s) args=(%s)", JSON.stringify(db), args);
       return db;
     }
   }
@@ -127,9 +142,10 @@ const resolvers = {
 const storeUploadGlobal = getStoreUpload(
   new Minio.Client({
     accessKey: MINIO_ACCESS_KEY,
-    endPoint: MINIO_ENDPOINT,
-    port: MINIO_PORT,
-    secretKey: MINIO_SECRET_KEY
+    endPoint: MINIO_SERVER_HOST,
+    port: MINIO_SERVER_PORT_NUMBER,
+    secretKey: MINIO_SECRET_KEY,
+    useSSL: false
   })
 );
 
